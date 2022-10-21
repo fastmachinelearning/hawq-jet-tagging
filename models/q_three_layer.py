81ff5814231598d0e1e74480d372fdab32da82f3
Javier Campos <jivcampos8@gmail.com>
2022-10-21 14:46:58 -0500

    Update and add base model

--- models/q_three_layer.py
+++ models/q_three_layer.py
@@ -1,37 +1,46 @@
 import torch.nn as nn
+import torch.nn.utils.prune as prune
 from hawq.utils.quantization_utils.quant_modules import QuantAct, QuantLinear
 
-from .three_layer import three_layer_mlp
+from .three_layer import get_model
 
 
-class QThreeLayer(nn.Module):
+class BaseModel(nn.Module):
+    def __init__(self, weight_precision, bias_precision):
+        super().__init__()
+        self.weight_precision = weight_precision
+        self.bias_precision = bias_precision
+
+    def init_dense(self, model, name):
+        layer = getattr(model, name)
+        quant_layer = QuantLinear(
+            weight_bit=self.weight_precision, bias_bit=self.bias_precision
+        )
+        quant_layer.set_param(layer)
+        setattr(self, name, quant_layer)
+
+    def prune(self):
+        for name, module in self.named_modules():
+            if isinstance(module, QuantLinear):
+                prune.random_unstructured(module, name="weight", amount=0.9)
+
+
+class QThreeLayer(BaseModel):
     def __init__(self, model, weight_precision=6, bias_precision=6, act_precision=6):
-        super(QThreeLayer, self).__init__()
+        super(QThreeLayer, self).__init__(weight_precision, bias_precision)
 
         self.quant_input = QuantAct(act_precision)
 
-        layer = getattr(model, "dense_1")
-        quant_layer = QuantLinear(weight_bit=weight_precision, bias_bit=bias_precision)
-        quant_layer.set_param(layer)
-        setattr(self, "dense_1", quant_layer)
+        self.init_dense(model, "dense_1")
         self.quant_act_1 = QuantAct(act_precision)
 
-        layer = getattr(model, "dense_2")
-        quant_layer = QuantLinear(weight_bit=weight_precision, bias_bit=bias_precision)
-        quant_layer.set_param(layer)
-        setattr(self, "dense_2", quant_layer)
+        self.init_dense(model, "dense_2")
         self.quant_act_2 = QuantAct(act_precision)
 
-        layer = getattr(model, "dense_3")
-        quant_layer = QuantLinear(weight_bit=weight_precision, bias_bit=bias_precision)
-        quant_layer.set_param(layer)
-        setattr(self, "dense_3", quant_layer)
+        self.init_dense(model, "dense_3")
         self.quant_act_3 = QuantAct(act_precision)
 
-        layer = getattr(model, "dense_4")
-        quant_layer = QuantLinear(weight_bit=weight_precision, bias_bit=bias_precision)
-        quant_layer.set_param(layer)
-        setattr(self, "dense_4", quant_layer)
+        self.init_dense(model, "dense_4")
 
         self.act = nn.ReLU()
         self.softmax = nn.Softmax(dim=1)
@@ -46,16 +55,149 @@ class QThreeLayer(nn.Module):
         x, act_scaling_factor = self.quant_act_2(x, act_scaling_factor)
 
         x = self.act(self.dense_3(x, act_scaling_factor))
-        x, act_scaling_factor = self.quant_act_3(
-            x,
-            act_scaling_factor,
-        )
+        x, act_scaling_factor = self.quant_act_3(x, act_scaling_factor)
 
         x = self.softmax(self.dense_4(x, act_scaling_factor))
         return x
 
 
-def q_three_layer(model=None):
+class QThreeLayer_BN(BaseModel):
+    def __init__(self, model, weight_precision=6, bias_precision=6, act_precision=6):
+        super(QThreeLayer_BN, self).__init__(weight_precision, bias_precision)
+
+        self.weight_precision = weight_precision
+        self.bias_precision = bias_precision
+
+        self.quant_input = QuantAct(act_precision)
+
+        self.init_dense(model, "dense_1")
+        self.bn1 = nn.BatchNorm1d(64)
+        self.quant_act_1 = QuantAct(act_precision)
+
+        self.init_dense(model, "dense_2")
+        self.bn2 = nn.BatchNorm1d(32)
+        self.quant_act_2 = QuantAct(act_precision)
+
+        self.init_dense(model, "dense_3")
+        self.bn3 = nn.BatchNorm1d(32)
+        self.quant_act_3 = QuantAct(act_precision)
+
+        self.init_dense(model, "dense_4")
+
+        self.act = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x, act_scaling_factor = self.quant_input(x)
+
+        x = self.dense_1(x, act_scaling_factor)
+        x = self.act(self.bn1(x))
+        x, act_scaling_factor = self.quant_act_1(x, act_scaling_factor)
+
+        x = self.dense_2(x, act_scaling_factor)
+        x = self.act(self.bn2(x))
+        x, act_scaling_factor = self.quant_act_2(x, act_scaling_factor)
+
+        x = self.dense_3(x, act_scaling_factor)
+        x = self.act(self.bn3(x))
+        x, act_scaling_factor = self.quant_act_3(x, act_scaling_factor)
+
+        x = self.softmax(self.dense_4(x, act_scaling_factor))
+        return x
+
+
+class QThreeLayer_Dropout(BaseModel):
+    def __init__(self, model, weight_precision=6, bias_precision=6, act_precision=6):
+        super(QThreeLayer_BN_Dropout, self).__init__(weight_precision, bias_precision)
+
+        self.weight_precision = weight_precision
+        self.bias_precision = bias_precision
+
+        self.quant_input = QuantAct(act_precision)
+
+        self.init_dense(model, "dense_1")
+        self.quant_act_1 = QuantAct(act_precision)
+
+        self.init_dense(model, "dense_2")
+        self.quant_act_2 = QuantAct(act_precision)
+
+        self.init_dense(model, "dense_3")
+        self.quant_act_3 = QuantAct(act_precision)
+
+        self.dropout = nn.Dropout(p=0.2)
+        self.act = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x, act_scaling_factor = self.quant_input(x)
+
+        x = self.dense_1(x, act_scaling_factor)
+        x = self.act(self.dropout(x))
+        x, act_scaling_factor = self.quant_act_1(x, act_scaling_factor)
+
+        x = self.dense_2(x, act_scaling_factor)
+        x = self.act(self.dropout(x))
+        x, act_scaling_factor = self.quant_act_2(x, act_scaling_factor)
+
+        x = self.dense_3(x, act_scaling_factor)
+        x = self.act(self.dropout(x))
+        x, act_scaling_factor = self.quant_act_3(x, act_scaling_factor)
+
+        x = self.softmax(self.dense_4(x, act_scaling_factor))
+        return x
+
+
+class QThreeLayer_BN_Dropout(BaseModel):
+    def __init__(self, model, weight_precision=6, bias_precision=6, act_precision=6):
+        super(QThreeLayer_BN_Dropout, self).__init__(weight_precision, bias_precision)
+
+        self.weight_precision = weight_precision
+        self.bias_precision = bias_precision
+
+        self.quant_input = QuantAct(act_precision)
+
+        self.init_dense(model, "dense_1")
+        self.bn1 = nn.BatchNorm1d(64)
+        self.quant_act_1 = QuantAct(act_precision)
+
+        self.init_dense(model, "dense_2")
+        self.bn2 = nn.BatchNorm1d(32)
+        self.quant_act_2 = QuantAct(act_precision)
+
+        self.init_dense(model, "dense_3")
+        self.bn3 = nn.BatchNorm1d(32)
+        self.quant_act_3 = QuantAct(act_precision)
+
+        self.dropout = nn.Dropout(p=0.2)
+        self.act = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x, act_scaling_factor = self.quant_input(x)
+
+        x = self.bn1(self.dense_1(x, act_scaling_factor))
+        x = self.act(self.dropout(x))
+        x, act_scaling_factor = self.quant_act_1(x, act_scaling_factor)
+
+        x = self.bn2(self.dense_2(x, act_scaling_factor))
+        x = self.act(self.dropout(x))
+        x, act_scaling_factor = self.quant_act_2(x, act_scaling_factor)
+
+        x = self.bn3(self.dense_3(x, act_scaling_factor))
+        x = self.act(self.dropout(x))
+        x, act_scaling_factor = self.quant_act_3(x, act_scaling_factor)
+
+        x = self.softmax(self.dense_4(x, act_scaling_factor))
+        return x
+
+
+def get_quantized_model(args, model=None):
     if model == None:
-        model = three_layer_mlp()
+        model = get_model(args)
+    if args.dropout and args.batch_norm:
+        return QThreeLayer_BN_Dropout(model)
+    if args.batch_norm:
+        return QThreeLayer_BN(model)
+    if args.dropout:
+        return QThreeLayer_Dropout()
     return QThreeLayer(model)
--- models/three_layer.py
+++ models/three_layer.py
@@ -1,3 +1,4 @@
+import torch 
 import torch.nn as nn
 
 
@@ -20,5 +21,40 @@ class ThreeLayerMLP(nn.Module):
         return self.softmax(self.dense_4(x))
 
 
-def three_layer_mlp():
+class ThreeLayer_BN(nn.Module):
+    def __init__(self):
+        super(ThreeLayer_BN, self).__init__()
+
+        self.dense_1 = nn.Linear(16, 64)
+        self.bn1 = nn.BatchNorm1d(64)
+
+        self.dense_2 = nn.Linear(64, 32)
+        self.bn2 = nn.BatchNorm1d(32)
+
+        self.dense_3 = nn.Linear(32, 32)
+        self.bn3 = nn.BatchNorm1d(32)
+
+        self.dense_4 = nn.Linear(32, 5)
+
+        self.act = nn.ReLU()
+        self.softmax = nn.Softmax(1)
+
+        # self.init_weights()
+
+    def init_weights(self):
+        for module in self.named_modules():
+            if isinstance(module, nn.Linear):
+                torch.nn.init.xavier_normal_(module.weight, torch.nn.init.calculate_gain("relu"))
+                torch.nn.init.xavier_normal_(module.bias, torch.nn.init.calculate_gain("relu"))
+
+    def forward(self, x):
+        x = self.act(self.bn1(self.dense_1(x)))
+        x = self.act(self.bn2(self.dense_2(x)))
+        x = self.act(self.bn3(self.dense_3(x)))
+        return self.softmax(self.dense_4(x))
+
+
+def get_model(args):
+    if args.batch_norm:
+        return ThreeLayer_BN()
     return ThreeLayerMLP()
