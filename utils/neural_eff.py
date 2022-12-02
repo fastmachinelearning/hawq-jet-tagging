6de4deef861e7d2439286a9b674fb1881cc8d32b
Javier Campos <jivcampos8@gmail.com>
2022-12-02 17:28:34 -0600

    Update neural_eff.py

--- utils/neural_eff.py
+++ utils/neural_eff.py
@@ -3,54 +3,51 @@ import numpy as np
 
 
 def calc_neural_efficieny(model, data_loader, args):
-    neural_eff = []
-    for inputs, targets in data_loader:
-        x1, x2, x3, x4 = get_activations(model, inputs.float(), args)
+    neural_eff_batch = []
+    for inputs, _ in data_loader:
+        a1, a2, a3, a4 = get_activations(model, inputs.float(), args)
 
-        e1 = np.count_nonzero(x1) / (64 * len(inputs))
-        e2 = np.count_nonzero(x2) / (32 * len(inputs))
-        e3 = np.count_nonzero(x3) / (32 * len(inputs))
-        e4 = np.count_nonzero(x4) / (5 * len(inputs))
+        # layer neural_eff = entropy/num_nodes
+        e1 = (np.count_nonzero(a1, axis=0) / len(inputs)).sum() / 64
+        e2 = (np.count_nonzero(a2, axis=0) / len(inputs)).sum() / 32
+        e3 = (np.count_nonzero(a3, axis=0) / len(inputs)).sum() / 32
+        e4 = (np.count_nonzero(a4, axis=0) / len(inputs)).sum() / 5
 
-        neural_eff.append(np.sqrt(np.sqrt(e1 * e2 * e3 * e4)))
-        # print(f"[Batch] Neural Efficiency: {neural_eff[-1]}")
+        neural_eff_batch.append(np.sqrt(np.sqrt(e1 * e2 * e3 * e4)))
 
-    logging.info(f"[Model] Neural Efficiency: {np.mean(np.array(neural_eff))}")
-    return np.mean(np.array(neural_eff))
+    neural_eff = np.mean(np.array(neural_eff_batch))
+    logging.info(f"[Model] Neural Efficiency: {neural_eff}")
+
+    return neural_eff
 
 
 def get_activations(model, data_in, args):
     x, act_scaling_factor = model.quant_input(data_in.float())
 
-    x1 = model.dense_1(x, act_scaling_factor)
+    x = model.dense_1(x, act_scaling_factor)
     if args.batch_norm:
-        x = model.bn1(x1)
-        x = model.act(x)
-    else:
-        x = model.act(x1)
-    x, act_scaling_factor = model.quant_act_1(x, act_scaling_factor)
+        x = model.bn1(x)
+    a1 = model.act(x)
+    x, act_scaling_factor = model.quant_act_1(a1, act_scaling_factor)
 
-    x2 = model.dense_2(x, act_scaling_factor)
+    x = model.dense_2(x, act_scaling_factor)
     if args.batch_norm:
-        x = model.bn2(x2)
-        x = model.act(x)
-    else:
-        x = model.act(x2)
-    x, act_scaling_factor = model.quant_act_2(x, act_scaling_factor)
+        x = model.bn2(x)
+    a2 = model.act(x)
+    x, act_scaling_factor = model.quant_act_2(a2, act_scaling_factor)
 
-    x3 = model.dense_3(x, act_scaling_factor)
+    x = model.dense_3(x, act_scaling_factor)
     if args.batch_norm:
-        x = model.bn3(x3)
-        x = model.act(x)
-    else:
-        x = model.act(x3)
-    x, act_scaling_factor = model.quant_act_3(x, act_scaling_factor)
+        x = model.bn3(x)
+    a3 = model.act(x)
+    x, act_scaling_factor = model.quant_act_3(a3, act_scaling_factor)
 
-    x4 = model.dense_4(x, act_scaling_factor)
+    x = model.dense_4(x, act_scaling_factor)
+    a4 = model.act(x)
 
     return (
-        x1.detach().numpy(),
-        x2.detach().numpy(),
-        x3.detach().numpy(),
-        x4.detach().numpy(),
+        a1.detach().numpy(),
+        a2.detach().numpy(),
+        a3.detach().numpy(),
+        a4.detach().numpy(),
     )
