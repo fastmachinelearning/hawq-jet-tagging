adc1268465ad09e88a0d890886d290c9ce5b8d8d
Javier Campos <jivcampos8@gmail.com>
2023-10-31 15:19:40 -0500

    Add econ data preprocessing

---
+++ training/econ/scripts/preprocess_data.sh
@@ -0,0 +1,4 @@
+python3 "$HAWQ_JET_TAGGING/training/econ/train.py" \
+    --process_data \
+    --data_file "$HAWQ_JET_TAGGING/data/econ/hgcal22data_signal_driven_ttbar_v11_nELinks5.npy" \
+    --data_dir "$HAWQ_JET_TAGGING/data/econ/" 
